fa27fc82f19dfb9c5d5b94f675bc87876da958d8
Ben Hauser <35276322+iamdefinitelyahuman@users.noreply.github.com>
2019-09-29 18:57:27 +0800

    Standard repo setup

---
+++ README.md
@@ -0,0 +1,37 @@
+# abi2solc
+
+[![Pypi Status](https://img.shields.io/pypi/v/abi2solc.svg)](https://pypi.org/project/abi2solc/) [![Build Status](https://img.shields.io/travis/com/iamdefinitelyahuman/abi2solc.svg)](https://travis-ci.com/iamdefinitelyahuman/abi2solc) [![Coverage Status](https://coveralls.io/repos/github/iamdefinitelyahuman/abi2solc/badge.svg?branch=master)](https://coveralls.io/github/iamdefinitelyahuman/abi2solc?branch=master)
+
+## Installation
+
+You can install the latest release via ``pip``:
+
+```bash
+$ pip install abi2solc
+```
+
+Or clone the repo and use ``setuptools``:
+
+```bash
+$ python setup.py install
+```
+
+## Usage
+
+TODO
+
+## Tests
+
+To run the test suite:
+
+```bash
+$ tox
+```
+
+## Development
+
+Comments, questions, criticisms and pull requests are welcomed! Feel free to open an issue if you encounter a problem or would like to suggest a new feature.
+
+## License
+
+This project is licensed under the [MIT license](LICENSE).
---
+++ setup.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="py-solc-ast",
+    version="0.1.0",
+    description="""Converts an ABI to a Solidity interface.""",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Ben Hauser",
+    author_email="b.hauser@zerolaw.tech",
+    url="https://github.com/iamdefinitelyahuman/abi2solc",
+    include_package_data=True,
+    py_modules=["abi2solc"],
+    setup_requires=[],
+    python_requires=">=3.6, <4",
+    install_requires=[],
+    license="MIT",
+    zip_safe=False,
+    keywords=["ethereum", "solidity", "solc", "abi"],
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+    ],
+)
